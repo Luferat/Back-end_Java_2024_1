fb929ff33b6da576bc671fc0e757e3578644e515
Luferat <contato@luferat.net>
2025-03-11 20:04:59 -0300

    Aula 05

---
+++ src/main/java/aula05/_03_heranca/Main.java
@@ -0,0 +1,9 @@
+package aula05._03_heranca;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.makeSound(); // Executa a versão da subclasse
+    }
+}
+
